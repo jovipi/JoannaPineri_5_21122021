aa713198a7833ecca45d1a5f4284dbc8d3425de4
Joanna Pineri <joannapineri@gmail.com>
2022-01-03 10:51:02 +0100

    Modification sur server.js rajout fetch

--- back/server.js
+++ back/server.js
@@ -43,3 +43,16 @@ server.on('listening', () => {
 });
 
 server.listen(port);
+
+fetch("https://api.github.com/users/jovipi/JoannaPineri_5_21122021")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(coucou) {
+    console.log(coucou);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
